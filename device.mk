c93d992ef8d616e47f159327c20fc73caf1e2ae9
Michael Bestas <mkbestas@lineageos.org>
2017-11-05 22:54:36 +0200

    r5: Add camera shims and libstlport
    * Moved from msm8939-common
    
    Change-Id: If4a1d7dbfde2ca6b0c798450903dc942c5518f64

--- device.mk
+++ device.mk
@@ -44,6 +44,12 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
+# Camera
+PRODUCT_PACKAGES += \
+    libshim_camera \
+    libboringssl-compat \
+    libstlport
+
 # Dalvik/HWUI configs
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)

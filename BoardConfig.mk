7ca69582420151fd2b5849dbb71eb428a712da03
sford <cjybyjk@gmail.com>
2018-03-31 20:06:48 +0800

    cm-13.0 device tree for OPPO 14045(R1C,R8207,R8205,R8200)

---
+++ BoardConfig.mk
@@ -0,0 +1,54 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+FORCE_32_BIT := true
+
+include device/oppo/msm8939-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/oppo/r1c
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := r1c,R1C,R8207,R8205,R8200
+
+# Assertions
+TARGET_BOARD_INFO_FILE ?= $(DEVICE_PATH)/board-info.txt
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_r1c
+TARGET_RECOVERY_DEVICE_MODULES := libinit_r1c
+
+# Kernel
+BOARD_DTBTOOL_ARGS := --force-v2
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset 0x02000000
+TARGET_KERNEL_CONFIG := lineageos_r1c_defconfig
+
+# Partition info
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_CACHEIMAGE_PARTITION_SIZE := 134217728
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1807745024
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 13218315264 # 13218331648 - 16384
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
+# inherit from the proprietary version
+-include vendor/oppo/r1c/BoardConfigVendor.mk

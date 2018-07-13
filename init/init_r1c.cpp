e53d30a1ea1deb09be0f010e58929e822b1c46fa
sford <cjybyjk@gmail.com>
2018-07-14 07:23:31 +0800

    init: update
    Change-Id: I26bc7888aeecface6e753130b2e15bda6a404e09

--- init/init_r1c.cpp
+++ init/init_r1c.cpp
@@ -1,6 +1,5 @@
 /*
    Copyright (c) 2016, The CyanogenMod Project
-             (c) 2017, The LineageOS Project
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -28,16 +27,27 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <cstdlib>
-#include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <android-base/file.h>
+#include <android-base/properties.h>
+#include <android-base/strings.h>
+
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
-#include "vendor_init.h"
 #include "property_service.h"
-#include "log.h"
-#include "util.h"
+#include "vendor_init.h"
+
+using android::base::GetProperty;
+using android::base::ReadFileToString;
+using android::base::Trim;
+using android::init::property_set;
 
 void property_override(char const prop[], char const value[])
 {
@@ -50,52 +60,33 @@ void property_override(char const prop[], char const value[])
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-static int read_file2(const char *fname, char *data, int max_size)
+static void init_alarm_boot_properties()
 {
-    int fd, rc;
+    char const *boot_reason_file = "/proc/sys/kernel/boot_reason";
+    char const *power_off_alarm_file = "/persist/alarm/powerOffAlarmSet";
+    std::string boot_reason;
+    std::string power_off_alarm;
+    std::string reboot_reason = GetProperty("ro.boot.alarmboot","");
 
-    if (max_size < 1)
-        return 0;
-
-    fd = open(fname, O_RDONLY);
-    if (fd < 0) {
-        ERROR("failed to open '%s'\n", fname);
-        return 0;
-    }
-
-    rc = read(fd, data, max_size - 1);
-    if ((rc > 0) && (rc < max_size))
-        data[rc] = '\0';
-    else
-        data[0] = '\0';
-    close(fd);
-
-    return 1;
-}
-
-void init_alarm_boot_properties()
-{
-    char const *alarm_file = "/proc/sys/kernel/boot_reason";
-    char buf[64];
-
-    if(read_file2(alarm_file, buf, sizeof(buf))) {
-
-    /*
-     * Setup ro.alarm_boot value to true when it is RTC triggered boot up
-     * For existing PMIC chips, the following mapping applies
-     * for the value of boot_reason:
-     *
-     * 0 -> unknown
-     * 1 -> hard reset
-     * 2 -> sudden momentary power loss (SMPL)
-     * 3 -> real time clock (RTC)
-     * 4 -> DC charger inserted
-     * 5 -> USB charger insertd
-     * 6 -> PON1 pin toggled (for secondary PMICs)
-     * 7 -> CBLPWR_N pin toggled (for external power supply)
-     * 8 -> KPDPWR_N pin toggled (power key pressed)
-     */
-        if(buf[0] == '3')
+    if (ReadFileToString(boot_reason_file, &boot_reason)
+            && ReadFileToString(power_off_alarm_file, &power_off_alarm)) {
+        /*
+         * Setup ro.alarm_boot value to true when it is RTC triggered boot up
+         * For existing PMIC chips, the following mapping applies
+         * for the value of boot_reason:
+         *
+         * 0 -> unknown
+         * 1 -> hard reset
+         * 2 -> sudden momentary power loss (SMPL)
+         * 3 -> real time clock (RTC)
+         * 4 -> DC charger inserted
+         * 5 -> USB charger inserted
+         * 6 -> PON1 pin toggled (for secondary PMICs)
+         * 7 -> CBLPWR_N pin toggled (for external power supply)
+         * 8 -> KPDPWR_N pin toggled (power key pressed)
+         */
+        if ((Trim(boot_reason) == "3" || reboot_reason == "true")
+                && Trim(power_off_alarm) == "1")
             property_set("ro.alarm_boot", "true");
         else
             property_set("ro.alarm_boot", "false");
@@ -105,24 +96,24 @@ void init_alarm_boot_properties()
 void vendor_load_properties()
 {
     char const *prj_file = "/proc/oppoVersion/prjVersion";
-    char prj_version[64];
+    std::string prj_version;
 
-    if (read_file2(prj_file, prj_version, sizeof(prj_version))) {
-        if (strstr(prj_version, "14045")) {
+    if (ReadFileToString(prj_file, &prj_version)) {
+        if (Trim(prj_version) == "14045") {
             property_override("ro.product.model", "R8207");
             property_set("ro.common.soft", "MSM_14045");
             property_override("ro.product.name", "R8207");
             property_override("ro.build.product", "R8207");
             property_override("ro.build.fingerprint", "OPPO/R8207/R1C:4.4.4/KTU84P/1390465867:user/release-keys");
             property_override("ro.build.description", "msm8916_32-user 4.4.4 KTU84P eng.root.20151213 release-keys");
-        } else if (strstr(prj_version, "14046")) {
+        } else if (Trim(prj_version) == "14046") {
             property_override("ro.product.model", "R8200");
             property_set("ro.common.soft", "MSM_14046");
             property_override("ro.product.name", "R8200");
             property_override("ro.build.product", "R8200");
             property_override("ro.build.fingerprint", "OPPO/R8200/R1C:4.4.4/KTU84P/1390465867:user/release-keys");
             property_override("ro.build.description", "msm8916_32-user 4.4.4 KTU84P eng.root.20150515 release-keys");
-        } else if (strstr(prj_version, "14047")) {
+        } else if (Trim(prj_version) == "14047") {
             property_override("ro.product.model", "R8205");
             property_set("ro.common.soft", "MSM_14047");
             property_override("ro.product.name", "R8205");
